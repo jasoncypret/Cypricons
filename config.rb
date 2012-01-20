c3df9500b5240d77c83425cfdae458ec538740c2
Jason Cypret <jason@jasoncypret.com>
2012-01-20 16:33:35 -0600

    Initial Commit

---
+++ config.rb
@@ -0,0 +1,29 @@
+# ==============================================================================
+# COMPASS PROJECT CONFIGURATION
+# ==============================================================================
+
+relative_assets = true
+line_comments = false
+output_style = :compressed
+disable_warnings = false
+
+# ==============================================================================
+# COMPASS SOURCE DIRECTORY CONFIGURATION
+# ==============================================================================
+
+css_dir = "css"
+sass_dir = "sass"
+fonts_dir = "fonts"
+images_dir = "images"
+javascripts_dir = "js"
+prefered_syntax = :sass
+
+# ==============================================================================
+# COMPASS TARGET DIRECTORY CONFIGURATION
+# ==============================================================================
+
+http_path = "../"
+http_css_path = http_path + "css"
+http_fonts_path = http_path + "fonts"
+http_images_path = http_path + "images"
+http_javascripts_path = http_path + "js"
